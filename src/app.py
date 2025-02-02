18bd74220f571cbe4f132b70c5f7bc301ffffa88
Facundo3007 <facundodutraextra@gmail.com>
2025-02-01 20:52:54 -0400

    gitignore updated and ai functioning properly(?)

--- src/app.py
+++ src/app.py
@@ -29,28 +29,47 @@ import torchhd
 import torch
 from variables import dimensions
 
+class someMethods:
+    def __init__(self):
+        pass
+
+
+    def loadHypervector(fileName):
+        return torch.load(fileName)
+    
+    def saveHypervector(hypervector, fileName):
+        torch.save(hypervector, fileName)
+
+    def generateHypervector(dimensions, fileName):
+        hv = torchhd.random(1, dimensions)
+        torch.save(hv, fileName)
+        return 
+    
+    def generateHypervectorIDs(amount , dimensions, fileName):
+        hvs = torchhd.random(amount, dimensions)
+        torch.save(hvs, fileName)
+        return hvs
+    
+
+
 # load the hypervectors
 try:
     print("Loading hypervectors")
-    nucleotide_A = torch.load("nucleotide_A.pt")
-    nucleotide_C = torch.load("nucleotide_C.pt")
-    nucleotide_G = torch.load("nucleotide_G.pt")
-    nucleotide_T = torch.load("nucleotide_T.pt")
-    specialChar = torch.load("specialChar.pt")
+    nucleotide_A = someMethods.loadHypervector("nucleotide_A.pt")
+    nucleotide_C = someMethods.loadHypervector("nucleotide_C.pt")
+    nucleotide_G = someMethods.loadHypervector("nucleotide_G.pt")
+    nucleotide_T = someMethods.loadHypervector("nucleotide_T.pt")
+    specialChar = someMethods.loadHypervector("specialChar.pt")
     print("Hypervectors loaded")
 except Exception as e:
     print("Error loading hypervectors")
+    print(e)
     print("Generating and saving hypervectors")
-    nucleotide_A = torchhd.random_hv(1, dimensions)
-    nucleotide_C = torchhd.random_hv(1, dimensions)
-    nucleotide_G = torchhd.random_hv(1, dimensions)
-    nucleotide_T = torchhd.random_hv(1, dimensions)
-    specialChar = torchhd.random_hv(1, dimensions)
-    torch.save(nucleotide_A, "nucleotide_A.pt")
-    torch.save(nucleotide_C, "nucleotide_C.pt")
-    torch.save(nucleotide_G, "nucleotide_G.pt")
-    torch.save(nucleotide_T, "nucleotide_T.pt")
-    torch.save(specialChar, "specialChar.pt")
+    nucleotide_A = someMethods.generateHypervector(dimensions, "nucleotide_A.pt")
+    nucleotide_C = someMethods.generateHypervector(dimensions, "nucleotide_C.pt")
+    nucleotide_G = someMethods.generateHypervector(dimensions, "nucleotide_G.pt")
+    nucleotide_T = someMethods.generateHypervector(dimensions, "nucleotide_T.pt")
+    specialChar = someMethods.generateHypervector(dimensions, "specialChar.pt")
     print("Hypervectors generated and saved")
 
 
@@ -64,6 +83,59 @@ for test in testList:
 
 print("Largest string: ", largestString)
 
+IDs = someMethods.generateHypervectorIDs(largestString, dimensions, "IDs.pt")
 
+# list of hypervectors for each DNA sequence
+testResults = []
 
+# generating the hypervectors for each DNA sequence
+for DNA_SEQUENCE in testList:
+    print("DNA_SEQUENCE: ", DNA_SEQUENCE)
 
+    if DNA_SEQUENCE[0] == "A":
+        firstLetter = torchhd.bind(nucleotide_A, IDs[0])
+    elif DNA_SEQUENCE[0] == "C":
+        firstLetter = torchhd.bind(nucleotide_C, IDs[0])
+    elif DNA_SEQUENCE[0] == "G":
+        firstLetter = torchhd.bind(nucleotide_G, IDs[0])
+    elif DNA_SEQUENCE[0] == "T":
+        firstLetter = torchhd.bind(nucleotide_T, IDs[0])
+    else:
+        firstLetter = torchhd.bind(specialChar, IDs[0])
+
+    if DNA_SEQUENCE[1] == "A":
+        nextLetter = torchhd.bind(nucleotide_A, IDs[1])
+    elif DNA_SEQUENCE[1] == "C":
+        nextLetter = torchhd.bind(nucleotide_C, IDs[1])
+    elif DNA_SEQUENCE[1] == "G":
+        nextLetter = torchhd.bind(nucleotide_G, IDs[1])
+    elif DNA_SEQUENCE[1] == "T":
+        nextLetter = torchhd.bind(nucleotide_T, IDs[1])
+    else:
+        nextLetter = torchhd.bind(specialChar, IDs[1])
+    sequenceHyperVector = torchhd.bundle(firstLetter, nextLetter)
+
+    for i in range(2, len(DNA_SEQUENCE)):
+        if DNA_SEQUENCE[i] == "A":
+            nextLetter = torchhd.bind(nucleotide_A, IDs[i])
+        elif DNA_SEQUENCE[i] == "C":
+            nextLetter = torchhd.bind(nucleotide_C, IDs[i])
+        elif DNA_SEQUENCE[i] == "G":
+            nextLetter = torchhd.bind(nucleotide_G, IDs[i])
+        elif DNA_SEQUENCE[i] == "T":
+            nextLetter = torchhd.bind(nucleotide_T, IDs[i])
+        else:
+            nextLetter = torchhd.bind(firstLetter, specialChar)
+        sequenceHyperVector = torchhd.bundle(sequenceHyperVector, nextLetter)
+    testResults.append(sequenceHyperVector)
+
+print("Test results: ", testResults)
+
+string = 10*" " + " ACAGTACAGT ACAGTCCAGT AAAGTCCAGT\n"
+
+for i in range(len(testResults)):
+    string += testList[i] + " "
+    for j in range(len(testResults)):
+        string += f"{(torchhd.hamming_similarity(testResults[i], testResults[j]))}" + 10*" "
+
+print(string)
